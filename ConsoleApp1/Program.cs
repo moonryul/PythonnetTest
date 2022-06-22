18b93a29d9754d1a56537d24737e08b27277fe54
moonryul <moon@sogang.ac.kr>
2022-06-22 09:27:49 +0900

    calling democs.py tested

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Python.Runtime;
-using Numpy;
+//using Numpy;
 //using Numpy.Models;
 
 //https://stackoverflow.com/questions/69339974/python-net-pythonengine-initialize-crashes-application-without-throwing-except
@@ -114,9 +114,8 @@ namespace PythonExecutor
                      Path.Combine(PYTHON_HOME, @"Lib\site-packages"),  
                     // 개인 패키지 폴더
                      @"D:\Dropbox\metaverse\PythonnetTest\ConsoleApp1\Python\MyLib",   // calculator.py module
-                     @"D:\Dropbox\metaverse\gesticulator",  // the root folder itself  under which demo package resides; demo package has demo.py module
-                      @"D:\Dropbox\metaverse\gesticulator\gesticulator",
-                       @"D:\Dropbox\metaverse\gesticulator\gesticulator\visualization"
+                     @"D:\Dropbox\metaverse\gesticulator",  // the project root folder  under which demo package resides; demo package has demo.py module
+                     @"D:\Dropbox\metaverse\gesticulator\gesticulator\visualization"   // the project root folder for motion visualization
 
                 }
             );
@@ -172,23 +171,50 @@ print(sys.version); ");
                 Console.WriteLine("\n\n new os.cwd={0}", cwd, "\n\n");
 
 
+
                 dynamic np = Py.Import("numpy");
+
+
+
+                Console.WriteLine("\n\n np.array test#\n");
+                //// create an NDarray from a C# array : https://github.com/SciSharp/Numpy.NET
+                var a = np.array(new[] { 2, 4, 9, 25 });
+                Console.WriteLine("a:{0} ", a);
+                // apply the square root to each element
+                var roots = np.sqrt(a);
+                Console.WriteLine("roots:{0}", roots);
+                // array([1.41421356, 2.        , 3.        , 5.        ])
+
+
+                Console.WriteLine("\n\n np.array test#\n");
+                //// create an NDarray from a C# array : https://github.com/SciSharp/Numpy.NET
+                var b = np.array(new List<float> { 2, 4, 9, 25 });
+                Console.WriteLine("b:{0} ", a);
+                // apply the square root to each element
+                roots = np.sqrt(b);
+                Console.WriteLine("roots:{0}", roots);
+                // array([1.41421356, 2.        , 3.        , 5.        ])
 
                 dynamic mod = Py.Import("examples.calculator");
 
 
-                dynamic pyresult = mod.add(1,2);
-               
-                int   result = (int)pyresult;  //  Microsoft.CSharp.RuntimeBinder.RuntimeBinderException:: Cannot convert type 'Python.Runtime.PyObject' to 'int'
+                dynamic pyresult = mod.add(1, 2);
+
+                int result = (int)pyresult;  //  Microsoft.CSharp.RuntimeBinder.RuntimeBinderException:: Cannot convert type 'Python.Runtime.PyObject' to 'int'
 
                 Console.WriteLine("\n pythion result:{0}", pyresult);
 
 
+
+
+                ///////////////////////////
+                //  (1)  The original version where the input audio file and text are  read from within python code
                 dynamic demo = Py.Import("demo.demo");   
 
 
                 dynamic motionPythonArray = demo.main();       // returns an 2 dim numpy array
 
+                
                 // https://community.intersystems.com/post/embedded-python-and-tcl-tkinter-windows:
 
                 // If your embedded python code calls tkinter library (which is used by a lot of graphic producing libraries, including matplotlib), you might get this error:
@@ -241,7 +267,7 @@ print(sys.version); ");
                 PyList motionPyList = PyList.AsList(motionPythonArray);   // https://stackoverflow.com/questions/63621696/python-net-convert-python-list-to-net-list-c
 
 
-            
+
                 //float[,] motionArray = (float[,])motionPythonList;     // does not work
 
                 Console.WriteLine("\n\n Print Python List  in Console#\n");
@@ -250,11 +276,11 @@ print(sys.version); ");
                 for (int i = 0; i < 520; i++)
                 {
                     Console.WriteLine("{0}: \n", i);
-                    for (int j = 0; j < 45;  j++)
+                    for (int j = 0; j < 45; j++)
                     {
                         //motionArray[i,j] = (float)motionPythonList[i][j];
                         Console.Write($"{motionPyList[i][j]} \t");
-                       
+
 
 
                     }
@@ -264,27 +290,75 @@ print(sys.version); ");
                 }
 
 
-                Console.WriteLine("\n\n np.array test#\n");
-                //// create an NDarray from a C# array : https://github.com/SciSharp/Numpy.NET
-                var a = np.array(new[] { 2, 4, 9, 25 });
-                Console.WriteLine("a:{0} ", a);
-                // apply the square root to each element
-                var roots = np.sqrt(a);
-                Console.WriteLine("roots:{0}", roots);
-                // array([1.41421356, 2.        , 3.        , 5.        ])
+
+                // (2) Test: the new version where the input audio and text are prepared within the csharp code
+
+                string text = "Deep learning is an algorithm inspired by how the human brain works, and as a result it's an algorithm which has no theoretical limitations on what it can do. The more data you give it and the more computation time you give it, the better it gets. The New York Times also showed in this article another extraordinary result of deep learning which I'm going to show you now. It shows that computers can listen and understand.";
+
+              //# audio, sample_rate = librosa.load(audio_filename) # sample_rate = 22050 discrete values  per second; 0.1 s = 1 frame => 2205 discrete time points per frame
+                dynamic librosa = Py.Import("librosa");  // import a package
+
+                dynamic audio_sample_rate = librosa.load(@"D:\Dropbox\metaverse\gesticulator\demo\input\jeremy_howard.wav"); // audio is an np.array [ shape=(n,) or (2,n)]
+                                                                                                                             //   array([ -4.756e-06,  -6.020e-06, ...,  -1.040e-06,   0.000e+00], dtype=float32)
 
 
-                Console.WriteLine("\n\n np.array test#\n");
-                //// create an NDarray from a C# array : https://github.com/SciSharp/Numpy.NET
-                var b = np.array(new List<float> { 2, 4, 9, 25 });
-                Console.WriteLine("b:{0} ", a);
-                // apply the square root to each element
-                roots = np.sqrt(b);
-                Console.WriteLine("roots:{0}", roots);
-                // array([1.41421356, 2.        , 3.        , 5.        ])
+                //     cf:  np.ndarray((2,), buffer = np.array([1, 2, 3]),
+                //           offset = np.int_().itemsize,
+                //           dtype = int) # offset = 1*itemsize, i.e. skip first element
+                //          => array([2, 3])
+
+                //numpy.array(object, dtype = None, *, copy = True, order = 'K', subok = False, ndmin = 0, like = None), where object should be array-like
+
+
+                // int sample_rate_cs = (int)sample_rate;
+                //Beginning with C# 3, variables that are declared at method scope can have an implicit "type" var.
+                //    An implicitly typed local variable is strongly typed just as if you had declared the type yourself,
+                //    but the compiler determines the type. The following two declarations of i are functionally equivalent:
+
+
+                //var i = 10; // Implicitly typed.
+                //int i = 10; // Explicitly typed.
+
+                //var xs = new List<int>(); <==>   List<int> xs = new List<int>();
+
+                //https://www.geeksforgeeks.org/dynamic-type-in-c-sharp/#:~:text=In%20C%23%204.0%2C%20a%20new,type%20at%20the%20run%20time.
+                //=> 
+                //In C# 4.0, a new type is introduced that is known as a dynamic type. It is used to avoid the compile-time type checking.
+                //    The compiler does not check the type of the dynamic type variable at compile time,
+                //    instead of this, the compiler gets the type at the run time. The dynamic type variable is created using dynamic keyword.
+
+                var audio = audio_sample_rate[0];
+                // np.array audio =  audio_sample_rate[0];  or dynamic audio = audio_sample_rate[0]
+                int sample_rate = audio_sample_rate[1];
+               
+                dynamic democs = Py.Import("demo.democs");
+
+                motionPythonArray = democs.main(audio, text, sample_rate);
+
+                //float[,] motionPythonNpArray = np.array(motionPythonArray);
+
+                //var  motionPythonNpArray = np.array(motionPythonArray);
+
+                motionPyList = PyList.AsList(motionPythonArray); 
+
+                Console.WriteLine("\n\n Print Python List  in Console:  Passing input to gesticulator from csharp\n");
+             
+
+                for (int i = 0; i < 520; i++)
+                {
+                    Console.WriteLine("{0}: \n", i);
+                    for (int j = 0; j < 45; j++)
+                    {
+                        //motionArray[i,j] = (float)motionPythonList[i][j];
+                        Console.Write($"{motionPyList[i][j]} \t");
 
 
 
+                    }
+
+                    Console.WriteLine("\n");
+
+                }
 
                 //Generate a sequence of gestures based on a sequence of speech features (audio and text)
 
